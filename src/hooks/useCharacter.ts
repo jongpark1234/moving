d45fe9c3ef48e8f98b5ea4d0f441f2b1e9248338
JongPark <jongpark1234@naver.com>
2023-06-06 04:16:41 +0900

    :recycle: class -> custom hook

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,22 +1,22 @@
 import { useCallback, useState } from 'react'
 
 const useCharacter: () => [number, number, (x: number, y: number) => void] = () => {
-    const WIDTH = 10
-    const HEIGHT = 10
-    const MOVEMENT = 10
-  
+    const [WIDTH] = useState(10);
+    const [HEIGHT] = useState(10);
+    const [MOVEMENT] = useState(10);
+
     const [xPos, setXPos] = useState(0)
     const [yPos, setYPos] = useState(0)
-  
+
     const posLimit = useCallback((pos: number, limit: number) => {
-      return Math.min(Math.max(pos, 0), limit)
+        return Math.min(Math.max(pos, 0), limit)
     }, [])
 
     const move = useCallback((dx: number, dy: number) => {
-        setXPos(posLimit(xPos + dx * MOVEMENT, window.innerWidth - WIDTH))
-        setYPos(posLimit(yPos + dy * MOVEMENT, window.innerHeight - HEIGHT))
-    }, [xPos, yPos])
-  
+        setXPos((prev) => posLimit(prev + dx * MOVEMENT, window.innerWidth - WIDTH))
+        setYPos((prev) => posLimit(prev + dy * MOVEMENT, window.innerHeight - HEIGHT))
+    }, [MOVEMENT, WIDTH, HEIGHT, posLimit])
+
     return [xPos, yPos, move]
 }
 
