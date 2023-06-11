1a27d976ade5175c865dc52fcd57b5af58814d9f
종박 <78944379+jongpark1234@users.noreply.github.com>
2023-06-11 23:44:37 +0900

    :fire: Remove recoil

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,11 @@
 import Router from './routers/router'
 import { BrowserRouter } from 'react-router-dom'
-import { RecoilRoot } from 'recoil'
 
 const App = () => {
     return (
-        <RecoilRoot>
-            <BrowserRouter>
-                <Router />
-            </BrowserRouter>
-        </RecoilRoot>
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
     )
 }
 
