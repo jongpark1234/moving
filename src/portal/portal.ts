643d03355ee5ebe1d32fca08d5ca2b1fc4ef6c87
종박 <78944379+jongpark1234@users.noreply.github.com>
2023-06-15 23:13:46 +0900

    :fire: Remove portal folder

--- src/portal/portal.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-import ReactDom from 'react-dom'
-
-const Portal = ({ children } : { children: ReactNode }) => {
-    const container = document.getElementById('skillContainer')
-    return ReactDom.createPortal(children, container as Element)
-}
-
-export default Portal
